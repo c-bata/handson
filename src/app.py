82599d2abee1393cab82ddb29890c4acc60df04f
c-bata <contact@c-bata.link>
2017-01-24 16:42:41 +0900

    Show sqlite query results in jinja2

--- src/app.py
+++ src/app.py
@@ -1,11 +1,26 @@
 from kobin import Kobin, TemplateResponse
+import sqlite3
 
 app = Kobin()
+
+SQLITE_PATH = 'db.sqlite3'
+_db = None
+
+
+def get_db():
+    global _db
+    if _db is None:
+        _db = sqlite3.connect(SQLITE_PATH)
+    return _db
 
 
 @app.route('/')
 def index() -> TemplateResponse:
-    return TemplateResponse('index.html')
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM tasks')
+    tasks = cur.fetchall()
+    cur.close()
+    return TemplateResponse('index.html', tasks=tasks)
 
 
 @app.route('/tasks/{task_id}')
