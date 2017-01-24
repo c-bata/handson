e7c7aef4685a9d0e4e7273aeabdec3e57b17ebd9
c-bata <contact@c-bata.link>
2017-01-24 16:05:46 +0900

    Implement routing

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
-from kobin import Kobin, TemplateResponse
+from kobin import Kobin, Response
 
 app = Kobin()
 
 
 @app.route('/')
-def index() -> TemplateResponse:
-    return TemplateResponse('index.html')
+def index() -> Response:
+    return Response('Hello World')
+
+
+@app.route('/tasks/{task_id}')
+def task_detail(task_id: int) -> Response:
+    return Response(f'Task {task_id}')
